e8f621f72b1602acee72d64657fdf2f2d88a97f1
Muzammilishtiaq <muzammil@devdesks.com>
2023-08-07 18:43:08 +0500

    privacy page updata

--- src/Web-screen-page/PrivacyPage.jsx
+++ src/Web-screen-page/PrivacyPage.jsx
@@ -10,7 +10,44 @@ export default class PrivacyPage extends Component {
     >
 <div className='w-[80vw] h-[80vh] bg-[#343a40] absolute top-[10%] left-[10%] overflow-hidden overflow-y-scroll px-24 py-5 rounded-md'>
     <h1 className='text-white text-5xl mb-4 font-mono font-semibold'>Privacy Policy</h1>
-    <p className='text-white'></p>
+    <div className="text-white">
+      <p className="">We look forward to your visit to our website w4free.com and your interest in our company and our offers. For external links to third-party content we accept no liability despite careful control of the content, as we have not initiated the transmission of this information, have not selected or changed the addressee of the information transmitted and the information transmitted itself. The protection of your personal data during the collection, processing and use during your visit to our website is important to us and takes place within the framework of the statutory provisions.
+<br />
+In the following, we will explain what information we collect during your visit to our website and how it is used:</p>
+{/* 1 */}
+<div className="1 ">
+  <h3>1. Collection and storage of personal data as well as the nature and purpose of their use</h3>
+  <p>a) When visiting the Website</p>
+  <p>Every time a customer (or other visitor) accesses our website, the internet browser used on your end device (computer, laptop, tablet, smartphone, etc.) automatically sends information to the server of out website. this information is temporily stored in so-called log file (log file).</p>
+  <p>The following data will be collected without your intervention and store until automated deletion:</p>
+  <p><span>.</span>IP address of the requestion computer as device ID or divdual device identifier and device type,</p>
+  <p><span>.</span>the name of the retrived file and the amount of data transferred as well as the data and timw of the rerival,</p>
+  <p><span>.message about successful call,</span></p>
+  <p><span>.requesting domain,</span></p>
+  <p><span>.description of the type of internet browser used and, if applicable, the operating system of your terminal and the name of your access provider</span></p>
+  <p><span>.Your browsing history and default weblog information, location data, including location data from your mobile device. Please note that for most mobile devices, you can control or disable the use of location services in the mobile device's settings menu.</span></p>
+  {/*  */}
+<p>Our legitimate interest in accordance. Art. 6 para. 1 sentence 1 lit. f DSGVO for the collection of data is based on the following purposes:</p>
+<p><span>.ensuring a smooth connection setup and comfortable use of the website,</span></p>
+<p><span>.Evaluation of system security and stability as well</span></p>
+<p><span>.for further administrative purposes.</span></p>
+{/*  */}
+<p>In no case we use the collected data for the purpose of drawing conclusions about you.</p>
+{/*  */}
+<p>b) By using our contact form <br />
+For questions of any kind, we offer you the opportunity to contact us via a form provided on the website. At least the indication of a valid e-mail address and your name is required so that we know from whom the request originates and in order to be able to answer them. Further information can be provided voluntarily.</p>
+{/*  */}
+<p>The data processing for the purpose of contacting us is in accordance with. Art. 6 para. 1 sentence 1 lit. a DSGVO based on your voluntarily granted consent.</p>
+<p>The personal data collected by us for the use of the contact form will be automatically deleted after completion of the request made by you.</p>
+<p>c) Upon conclusion of a contractual relationship</p>
+{/*  */}
+
+<p>When concluding a contractual relationship on our website (eg registration on the website / platform), we ask you for the following personal data:</p>
+<p><span>.Data that personally identifies you, such as last name, first name and e-mail address</span></p>
+<p><span>.</span></p>
+<p><span>.</span></p>
+</div>
+    </div>
 </div>
     </div>
     )
